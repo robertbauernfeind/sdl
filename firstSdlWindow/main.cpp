c11f1a42ad148a4f90bfbdb1427a958620df08e7
Robert Bauernfeind <r.bauernfeind@braun.at>
2025-03-26 11:28:58 +0100

    init

---
+++ firstSdlWindow/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "SDL3/SDL.h"
+// #include <SDL3/SDL_main.h>
+
+using namespace std;
+
+int main() {
+    SDL_Log("%s", "Hello World from my first SDL3 project!");
+    return 0;
+}
